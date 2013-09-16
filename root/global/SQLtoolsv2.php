8c1e0dc15201a5e7414652f816e5ee0c99ce2c17
Victor van Lange <v.vanlange@gmail.com>
2013-09-16 22:41:34 +0200

    klein beetje comment invoegen

--- root/global/SQLtoolsv2.php
+++ root/global/SQLtoolsv2.php
@@ -5,30 +5,38 @@
 			
 			switch($mode) {
 				case 1:
+					//toevoegen van een wissel aan de DB
 					$sql = "INSERT INTO Wissels (leerlingnummer, datum, dag, vanuur, naaruur, commentaar, OK) VALUES (:leerlingnummer, :datum, :dag, :vanuur, :naaruur, :commentaar, 0)";
 					break;
 				case 2:
+					//ophalen van wissels op basis van enkel het LN
 					$sql = "SELECT * FROM Wissels WHERE leerlingnummer = :leerlingnummer";
 					break;
 				case 3:
+					//ophalen van alle wissels op een bepaalde datum, gesorteerd op het uur waar het vandaan komt en waar het heen moet
 					$sql = "SELECT * FROM Wissels WHERE datum = :datum ORDER BY vanuur, naaruur";
 					break;
 				case 4:
+					//ophalen van één bepaalde wissel van een persoon
 					$sql = "SELECT * FROM Wissels WHERE id=:id AND leerlingnummer=:leerlingnummer";
 					break;
 				case 5:
+					//het veranderen van een wissel
 					$sql = "UPDATE Wissels SET vanuur=:vanuur, naaruur=:naaruur, commentaar=:commentaar, OK=0 WHERE leerlingnummer=:leerlingnummer AND id=:id";
 					break;
 				case 6:
+					//het verwijderen van een wissel
 					$sql = "DELETE FROM Wissels WHERE leerlingnummer=:leerlingnummer AND id=:id";
 					break;
 				case 7:
+					//het goedkeuren van een wissel
 					$sql = "UPDATE Wissels SET OK=:OK WHERE id=:id";
 					break;
 				default:
 					try{throw new Exception('no mode set!');} catch (Exception $r) {echo $r->getMessage();}
 			}
 			$stmt = $db->prepare($sql);
+			// per mode kijken of er een bepaalde parameter gebonden moet worden.
 			if ($mode == 1 || $mode == 2 || $mode == 4 || $mode == 5 || $mode == 6) $stmt->bindParam(':leerlingnummer', $lrlngnr);
 			if ($mode == 1 || $mode == 3) $stmt->bindParam(':datum', $datum);
 			if ($mode == 1) $stmt->bindParam(':dag', $dag);
@@ -38,7 +46,9 @@
 			if ($mode == 4 || $mode == 5 || $mode == 6 || $mode == 7) $stmt->bindParam(':id', $id);
 			if ($mode == 7) $stmt->bindParam(':OK', $accept);
 			$resultaat = $stmt->execute();
+			//eventuele speciale gevallen filteren waarbij alleen een los record of een subtabel eruit gehaalt moet worden.
 			switch($mode) {
+				//de fallthrough is hier gebruikt omdat beide waarden (2,3) de zelfde behandeling nodig hebben.
 				case 2:
 				case 3:
 					$table = $stmt->fetchAll();
